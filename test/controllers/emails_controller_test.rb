c812d53d0bb1afa36da6dd32a00cb4f6139a7225
samory-ka <samory.ka@doctolib.com>
2024-01-05 17:16:13 +0100

    add controller tests

--- test/controllers/emails_controller_test.rb
+++ test/controllers/emails_controller_test.rb
@@ -8,6 +8,20 @@ class EmailsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "samoryka@unkowndomain.com", response['email']
   end
 
+  test 'returns email for first_name_last_name format' do
+    post '/derive', params: { full_name: 'Samory Ka', company_domain: 'babbel.com' }, as: :json
+
+    response = JSON.parse(@response.body)
+    assert_equal "ska@babbel.com", response['email']
+  end
+
+  test 'returns email for first_name_initial_last_name format' do
+    post '/derive', params: { full_name: 'Samory Ka', company_domain: 'google.com' }, as: :json
+
+    response = JSON.parse(@response.body)
+    assert_equal "samoryka@google.com", response['email']
+  end
+
   test 'ignores middle name' do
     post '/derive', params: { full_name: 'Samory Kabir Ka', company_domain: 'unkowndomain.com' }, as: :json
 
